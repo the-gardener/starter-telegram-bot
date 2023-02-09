fc4a706839b7586409ce42d384360529970a8011
Tuanka <tuanka@vn.ibm.com>
2023-02-09 09:25:59 +0700

    test

--- src/bot.ts
+++ src/bot.ts
@@ -69,19 +69,18 @@ const replyToCommand = async (ctx: any) => {
 bot.on("message", replyToCommand);
 
 // Start the server
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   // Use Webhooks for the production server
   const app = express();
   app.use(express.json());
-  // app.use(webhookCallback(bot, "express"));
-  app.get('/', webhookCallback(bot, 'express'))
-  const url = `${process.env.WEBHOOK_URL}`;
-  bot.api.setWebhook(url);
+  app.use('/', webhookCallback(bot, "express"));
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
+    const url = `${process.env.WEBHOOK_URL}`;
+    bot.api.setWebhook(url);
     console.log(`Bot listening on port ${PORT}`);
   });
-} else {
+/* } else {
   // Use Long Polling for development
   bot.start();
-}
+} */
